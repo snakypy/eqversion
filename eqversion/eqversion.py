7ac7d810fe7a51493f8af0d3250d34910c85d0a9
William C. Canin <william.costa.canin@gmail.com>
2020-03-29 17:15:28 -0300

    :sparkles: :bug: Add Click package for CLI and and optional main package name

--- eqversion/eqversion.py
+++ eqversion/eqversion.py
@@ -1,30 +1,29 @@
 import os
 import re
-import argparse
+import click
 import snakypy
 import tomlkit
+from eqversion import __version__
 
 
 class EQVersion:
-    def __init__(self, root, package, tool):
+    def __init__(self, root):
         self.root = root
-        self.package = package
-        self.tool = tool
-        self.init_file = os.path.join(self.root, self.package, "__init__.py")
         self.pyproject = os.path.join(self.root, "pyproject.toml")
 
-    def version_pyproject(self):
+    def version_name_pyproject(self):
         if os.path.isfile(self.pyproject):
             r_file = snakypy.file.read(self.pyproject)
-            parsed = tomlkit.parse(r_file)["tool"][self.tool]
-            return parsed["version"]
+            parsed = tomlkit.parse(r_file)["tool"]["poetry"]
+            return parsed["version"], parsed["name"].replace("-", "_")
         return
 
-    def read_init(self):
-        if os.path.isdir(os.path.join(self.root, self.package)) and os.path.isfile(
-            self.init_file
+    def read_init(self, package):
+        init_file = os.path.join(self.root, package, "__init__.py")
+        if os.path.isdir(os.path.join(self.root, package)) and os.path.isfile(
+            init_file
         ):
-            file_content = snakypy.file.read(self.init_file)
+            file_content = snakypy.file.read(init_file)
             m = re.search(r"__version__ = .*", file_content)
             if m is not None:
                 line_version = m.group(0)
@@ -34,36 +33,53 @@ class EQVersion:
             return
         return
 
-    def match(self):
-        if self.read_init() and self.version_pyproject():
-            line_version = self.read_init()[1]
-            new_line_version = f'__version__ = "{self.version_pyproject()}"'
+    def match(self, package):
+        init_file = os.path.join(self.root, package, "__init__.py")
+        if self.read_init(package) and self.version_name_pyproject()[0]:
+            line_version = self.read_init(package)[1]
+            new_line_version = f'__version__ = "{self.version_name_pyproject()[0]}"'
             new_init_file = re.sub(
-                rf"{line_version}", new_line_version, self.read_init()[2], flags=re.M
+                rf"{line_version}",
+                new_line_version,
+                self.read_init(package)[2],
+                flags=re.M,
             )
-            if self.read_init()[0] != self.version_pyproject():
+            if self.read_init(package)[0] != self.version_name_pyproject()[0]:
                 print(
-                    f"File: {self.package}/__init__.py: "
-                    f"{self.read_init()[0]} >> {self.version_pyproject()}"
+                    f"File: {package}/__init__.py: "
+                    f"{self.read_init(package)[0]} >> "
+                    f"{self.version_name_pyproject()[0]}"
                 )
-                snakypy.file.create(new_init_file, self.init_file, force=True)
+                snakypy.file.create(new_init_file, init_file, force=True)
             else:
                 print("All the same!")
             return True
         return
 
 
-def options():
-    parser = argparse.ArgumentParser(
-        description="EQversion keeps the versions of pyproject.toml and __init__.py\
-                     the same."
-    )
-    parser.add_argument(
-        "package", metavar="PACKAGE", type=str, help="Name of the main package."
-    )
-    args = parser.parse_args()
-    return args
+def print_version(ctx, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(f"Version {__version__}")
+    ctx.exit()
 
 
-def main():
-    EQVersion(os.getcwd(), options().package, "poetry").match()
+@click.command()
+@click.option("--package", help="Receives the name of the main package.")
+@click.option(
+    "--version",
+    is_flag=True,
+    callback=print_version,
+    expose_value=False,
+    is_eager=True,
+    help="Show the current version.",
+)
+def main(package):
+    """EQversion maintains the same versions of pyproject.toml and
+    __init__.py (from the main package) of projects using Poetry."""
+    root = os.getcwd()
+    eqversion = EQVersion(root)
+    if package is not None:
+        eqversion.match(package)
+    else:
+        eqversion.match(eqversion.version_name_pyproject()[1])
